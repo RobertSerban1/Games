f35f462957b8559bf1ec6631e2769d838e9dee66
Robert Serban <serbanrobert43@yahoo.com>
2025-05-15 16:07:57 +0300

    popular+new+upcoming games

--- src/actions/gamesAction.jsx
+++ src/actions/gamesAction.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { popularGamesURL, upcomingGamesURL, newGamesURL } from "../api";
 
 //Action creator
 
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesURL()); // apelează API-ul
+  const newGamesData = await axios.get(newGamesURL()); // apelează API-ul
+  const upComingData = await axios.get(upcomingGamesURL()); // apelează API-ul
   dispatch({
     type: "FETCH_GAMES",
     payload: {
       popular: popularData.data.results, // trimite datele reale ca payload
+      upcoming: upComingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
