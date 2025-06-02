80ceeda1cf77e0b78614dd3535661d0f1522d8df
Robert Serban <serbanrobert43@yahoo.com>
2025-06-02 18:30:27 +0300

    game details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 //Components and pages
 import Home from "./pages/Home";
 import GlobalStyles from "./components/GlobalStyles";
